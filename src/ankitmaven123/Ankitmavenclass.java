1930800beadaecd49cf013b5bba277d8b3ed1bd7
Genius <Genius@DESKTOP-3V3M87M>
2024-01-30 20:33:34 +0530

    new class created by ankit

---
+++ src/ankitmaven123/Ankitmavenclass.java
@@ -0,0 +1,10 @@
+package ankitmaven123;
+
+public class Ankitmavenclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("code by ankit");
+	}
+
+}
